dac7e49291e02b0989d81d396429c2f0445e453d
kangninathan-glitch <kangninathan@gmail.com>
2026-01-07 15:07:06 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template_string, render_template, jsonify, request, redirect, url_for, session
-from flask import render_template
 from flask import json
 from urllib.request import urlopen
 from werkzeug.utils import secure_filename
@@ -13,8 +12,8 @@ def est_authentifie():
     return session.get('authentifie')
 
 @app.route('/')
-def hello_world():
-    return render_template('hello.html')
+def index():
+    return redirect(url_for('formulaire_client'))
 
 @app.route('/lecture')
 def lecture():
