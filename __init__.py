3924cc624c8483ed0dcd6e3976075786cd15b7d6
kangninathan-glitch <kangninathan@gmail.com>
2026-01-28 15:34:05 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -271,10 +271,6 @@ def api_return():
     conn.close()
     return jsonify({"ok": True})
 
-@app.route("/clients")
-def clients_home():
-    return redirect(url_for('formulaire_client'))
-
                                                                                                                                        
 if __name__ == "__main__":
   app.run(debug=True)
